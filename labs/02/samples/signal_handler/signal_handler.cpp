ff07cafa8272e46e0bad06f4ecf1ae92b5a1c8a6
Alexey Malov <malov.aleksey@gmail.com>
2026-09-22 20:23:26 +0300

    Обновил лекцию "процессы" и примеры к ней

---
+++ labs/02/samples/signal_handler/signal_handler.cpp
@@ -0,0 +1,28 @@
+﻿#include <csignal>
+
+namespace
+{
+volatile sig_atomic_t stop_requested = 0;
+
+void HandleSigterm(int)
+{
+	stop_requested = 1;
+}
+} // namespace
+
+int main()
+{
+	struct sigaction sa{};
+	sa.sa_handler = HandleSigterm;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+
+	sigaction(SIGTERM, &sa, nullptr);
+
+	while (!stop_requested)
+	{
+		// основная работа
+	}
+
+	// корректное завершение
+}
